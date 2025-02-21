2ecb8fe6228d36c2364e95994d74d76cd7393175
LUAN HENRIQUE AFONSO <0220482313046@fatectq.local>
2025-02-21 20:41:34 -0300

    feat: added basic variables

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,7 +1,7 @@
 import "./index.scss";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/App/index.tsx";
+import App from "./components/app/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
