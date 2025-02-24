4281422b2baf94b1b3acd33e4f686b6aef2bbf11
luanAfons0 <luan.hmatao@gmail.com>
2025-02-23 21:55:19 -0300

    feat: created not found page

---
+++ front-end/src/components/NotFound/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+export default function NotFoundComponent() {
+  return (
+    <div className={styles.container}>
+      <h1>Oops! Page not found!</h1>
+    </div>
+  );
+}
---
+++ front-end/src/pages/GamePage.tsx
@@ -0,0 +1,12 @@
+import GameSection from "../components/GameSection";
+import PageDisplay from "../components/PageDisplay";
+import Header from "../components/Header";
+
+export default function GamePage() {
+  return (
+    <PageDisplay>
+      <Header />
+      <GameSection />
+    </PageDisplay>
+  );
+}
