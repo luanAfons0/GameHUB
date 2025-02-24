62a1f72489e7fbeaee67d9a76a4da56cde802ac4
luanAfons0 <luan.hmatao@gmail.com>
2025-02-23 22:31:22 -0300

    update: better home page

--- front-end/src/pages/home.tsx
+++ front-end/src/pages/home.tsx
@@ -1,10 +1,22 @@
 import PageDisplay from "../components/PageDisplay";
+import Container from "../components/Container";
+import FlexRow from "../components/FlexRow";
+import Header from "../components/Header";
+import Link from "../components/Link";
 
 export default function Home() {
   return (
     <PageDisplay>
-      <h1>Here is the home page!</h1>
-      <p>The basic content of the site is here.</p>
+      <Header />
+      <Container>
+        <FlexRow>
+          <div>
+            <h1>Here is the home page!</h1>
+            <p>The basic content of the site is here.</p>
+            <Link text="Go to all games" url="/game-section" />
+          </div>
+        </FlexRow>
+      </Container>
     </PageDisplay>
   );
 }
